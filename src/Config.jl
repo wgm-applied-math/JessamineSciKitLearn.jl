db3b7f70ea645688e87807e7c20b21b275d2e4aa
W. Garrett Mitchener <garrett.mitchener@gmail.com>
2026-03-13 09:36:35 -0400

    Flexible specification of operation inventory

--- src/Config.jl
+++ src/Config.jl
@@ -19,6 +19,34 @@ function make_problem_for(input_size, opts)
     lambda_operands = get(opts, "lambda_operand", 1e-10)
 
     # Operator inventory
-    op_inventory_key = get(opts, "op_inventory", "Polynomial")
+    op_inventory_spec = get(opts, "op_inventory", "Polynomial")
+    op_inventory = get_or_build_op_inventory(op_inventory_spec)
+end
 
+function get_or_build_op_inventory(op_inventory_spec)
+    op_subspecs = split_symbols(op_inventory_spec)
+    if isempty(op_subspecs)
+        return get_op_inventory("Polynomial")
+    else
+        if length(op_subspecs) == 1
+            op_inventory_lookup = get_op_inventory(op_subspecs[1])
+            if op_inventory_lookup.found
+                return op_inventory_lookup.inventory
+            else
+                op_inventory_build = build_op_inventory(op_subspecs)
+                if !isempty(op_inventory_build.unknown)
+                    @warn "get_or_build_op_inventory: Ignoring unrecognized operation inventory specs: $(join(op_inventory_build.unknown))"
+                end
+                if isempty(op_inventory_build.inventory)
+                    @warn "get_or_build_op_inventory: No operations specified, using default polynomial inventory"
+                    return get_op_inventory("Polynomial")
+                end
+                return op_inventory_build.inventory
+            end
+        end
+    end
+end
+
+function split_symbols(s)
+    [m.match for m in eachmatch(r"""[^][,'"[:space:]]+""", s)]
 end
